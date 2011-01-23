6bb31ad5adc561bdc074e236f75b4c9cb4cad77a
Ronald Mathies <ronald@sodeso.nl>
2011-01-23 20:43:03 +0100

    Added new demo, modified some documentation and added the group attribute to the shapes.

---
+++ Chipmunk-Wrapper/Classes/Demos/Chipmunk-Demos/CarDemo.m
@@ -0,0 +1,78 @@
+//
+//  CarDemo.m
+//  Chipmunk
+//
+//  Created by Ronald Mathies on 12/27/10.
+//  Copyright 2010 Sodeso. All rights reserved.
+//
+
+#import "CarDemo.h"
+
+// --- Static variables ----------------------------------------------------------------------------
+
+// --- Static inline methods -----------------------------------------------------------------------
+
+// --- private interface ---------------------------------------------------------------------------
+
+@interface CarDemo ()
+
+- (void)initializeChipmunkObjects;
+
+@end
+
+// --- Class implementation ------------------------------------------------------------------------
+
+@implementation CarDemo
+
+- (void) initializeChipmunkObjects {
+	// Create chassis
+	CMBody *chassis = [mSpace addBodyWithMass:5 moment:0.5];
+	[chassis setPosition:cpv(80, 110)];
+	[chassis addToSpace];
+
+	CMShape *shape = [chassis addPolyWithPoints:4 vertices:
+					  cpv(-40,-15),
+					  cpv(-40, 15),
+					  cpv( 40, 15),
+					  cpv( 40,-15)];
+
+	[shape setElasticity:0.0];
+	[shape setFriction:0.7];
+	[shape setGroup:1];
+	[shape addToSpace];
+	
+	// Create the first wheel
+	CMBody *wheel1 = [mSpace addBodyWithMass:1 moment:0.5];
+	[wheel1 setPosition:cpv(50,160)];
+	[wheel1 addToSpace];
+	
+	CMShape *wheel1Shape = [wheel1 addCircleWithRadius:15.0f];
+	[wheel1Shape setElasticity:0.0];
+	[wheel1Shape setFriction:0.7];
+	[wheel1Shape setGroup:1];
+	[wheel1Shape addToSpace];
+	
+	// Create the second wheel
+	CMBody *wheel2 = [mSpace addBodyWithMass:1 moment:0.5];
+	[wheel2 setPosition:cpv(110,160)];
+	[wheel2 addToSpace];
+	
+	CMShape *wheel2Shape = [wheel2 addCircleWithRadius:15.0f];
+	[wheel2Shape setElasticity:0.0];
+	[wheel2Shape setFriction:0.7];
+	[wheel2Shape setGroup:1];
+	[wheel2Shape addToSpace];
+	
+	CMGrooveJointConstraint *grooveJointConstraint1 = [chassis addGrooveJointConstraintWithBody:wheel1 grooveA:cpv(-30, -10) grooveB:cpv(-30, 40) anchor1:cpvzero];
+	[grooveJointConstraint1 addToSpace];
+	CMGrooveJointConstraint *grooveJointConstraint2 = [chassis addGrooveJointConstraintWithBody:wheel2 grooveA:cpv( 30, -10) grooveB:cpv( 30, 40) anchor1:cpvzero];
+	[grooveJointConstraint2 addToSpace];
+	
+	CMDampedSpringConstraint *dampedSpringConstraint1 = [chassis addDampedSpringConstraintWithBody:wheel1 anchor1:cpv(-30,0) anchor2:cpvzero restLength:50.0f stiffness:20.0f damping:1.5f];
+	[dampedSpringConstraint1 addToSpace];
+	CMDampedSpringConstraint *dampedSpringConstraint2 = [chassis addDampedSpringConstraintWithBody:wheel2 anchor1:cpv( 30,0) anchor2:cpvzero restLength:50.0f stiffness:20.0f damping:1.5f];
+	[dampedSpringConstraint2 addToSpace];
+	
+}
+
+@end
